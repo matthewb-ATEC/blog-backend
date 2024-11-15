9a4c2f027936c62c0d7a80f745189db5db7875d8
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-15 15:22:09 -0500

    13.16

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 app.use(express.json())
 
@@ -17,6 +18,7 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
