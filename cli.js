dfb90115dd5a4c5efed2d2f894780c70dff0bf3f
matthewjburton <mattjburton64@gmail.com>
2024-11-15 09:17:12 -0500

    13.3

---
+++ cli.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
+const main = async () => {
+  try {
+    const blogs = await sequelize.query('SELECT * FROM blogs', {
+      type: QueryTypes.SELECT,
+    })
+    blogs.forEach((blog) => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    })
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()
--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query('SELECT * FROM notes', {
-    type: QueryTypes.SELECT,
-  })
-  res.json(notes)
-})
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
