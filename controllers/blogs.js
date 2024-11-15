71bd4b6a62898131979ee44a4ae08777a0ccf697
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-15 14:42:14 -0500

    13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
-const { tokenExtractor } = require('../util/middleware')
-
 const { Blog, User } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
+const { Op } = require('sequelize')
 
 const blogFinder = async (req, res, next) => {
   const blog = await Blog.findByPk(req.params.id)
@@ -13,13 +13,23 @@ const blogFinder = async (req, res, next) => {
 }
 
 blogsRouter.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search,
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name'],
     },
+    where,
   })
+
   res.status(200).send(blogs)
 })
 
