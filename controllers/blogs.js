1dff993402b4cb45c0a45b87faea1d05a4245666
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-15 11:07:53 -0500

    13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,65 +3,40 @@ const blogsRouter = require('express').Router()
 const { Blog } = require('../models')
 
 const blogFinder = async (req, res, next) => {
-  try {
-    const blog = await Blog.findByPk(req.params.id)
-    if (!blog) {
-      return res.status(404).json({ error: 'Blog not found' })
-    }
-    req.blog = blog
-    next()
-  } catch (error) {
-    return res.status(400).json({ error })
+  const blog = await Blog.findByPk(req.params.id)
+  if (!blog) {
+    return res.status(404).json({ error: 'Blog not found' })
   }
+  req.blog = blog
+  next()
 }
 
 blogsRouter.get('/', async (req, res) => {
-  try {
-    const blogs = await Blog.findAll()
-    if (blogs) res.status(200).send(blogs)
-    else res.status(404).end()
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  const blogs = await Blog.findAll()
+  if (blogs) res.status(200).send(blogs)
+  else res.status(404).end()
 })
 
 blogsRouter.get('/:id', blogFinder, async (req, res) => {
-  try {
-    if (req.blog) res.status(200).send(req.blog)
-    else res.status(404).end()
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  if (req.blog) res.status(200).send(req.blog)
+  else res.status(404).end()
 })
 
 blogsRouter.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    if (blog) res.status(200).send(blog)
-    else res.status(404).end()
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  const blog = await Blog.create(req.body)
+  if (blog) res.status(200).send(blog)
+  else res.status(404).end()
 })
 
 blogsRouter.put('/:id', blogFinder, async (req, res) => {
-  try {
-    req.blog.likes += 1
-    await req.blog.save()
-
-    res.status(201).send(req.blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  req.blog.likes += 1
+  await req.blog.save()
+  res.status(201).send(req.blog)
 })
 
 blogsRouter.delete('/:id', blogFinder, async (req, res) => {
-  try {
-    await Blog.destroy({ where: { id: id } })
-    res.status(204).end()
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  await Blog.destroy({ where: { id: id } })
+  res.status(204).end()
 })
 
 module.exports = blogsRouter
