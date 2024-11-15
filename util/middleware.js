3358d1d175daaa1ad704b2e5dce67ca23f41665a
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-15 12:00:21 -0500

    13.9

--- util/middleware.js
+++ util/middleware.js
@@ -2,6 +2,11 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   console.error(error.name)
 
+  if (error.name === 'SequelizeValidationError') {
+    const errorMessages = error.errors.map((err) => err.message)
+    return response.status(400).json({ error: errorMessages })
+  }
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
