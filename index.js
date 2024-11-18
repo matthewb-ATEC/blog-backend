26efddf1e6b613c00d963bd8ea619f0c30aacdf0
matthewjburton <mattjburton64@gmail.com>
2024-11-18 14:45:26 -0500

    13.19 & 13.20

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const authorsRouter = require('./controllers/authors')
+const readingListsRouter = require('./controllers/readingLists')
 
 app.use(express.json())
 
@@ -19,6 +20,7 @@ app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/authors', authorsRouter)
+app.use('/api/readinglists', readingListsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const User = require('./user')
 const Blog = require('./blog')
+const UserBlogs = require('./userBlogs')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: UserBlogs, as: 'reading_list' })
+Blog.belongsToMany(User, { through: UserBlogs, as: 'users_reading' })
+
 module.exports = {
   Blog,
   User,
+  UserBlogs,
 }
